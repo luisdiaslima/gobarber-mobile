2d16605991a68c540e11105283f59470d58966a1
Luis <luisdiaslima25@gmail.com>
2020-09-05 16:59:30 -0300

    signIn|signUp|SignOut completed

---
+++ src/hooks/auth.tsx
@@ -0,0 +1,92 @@
+import React, {
+  createContext,
+  useCallback,
+  useContext,
+  useState,
+  useEffect,
+} from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+
+import api from '../services/api';
+
+// Interface para o método SignIn
+interface SignInCredentials {
+  email: string;
+  password: string;
+}
+
+// Interface do meus estado
+interface AuthState {
+  token: string;
+  user: object;
+}
+
+interface AuthContextData {
+  user: object;
+  signIn(credentails: SignInCredentials): Promise<void>;
+  signOut(): void;
+  loading: boolean;
+}
+
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+
+export const AuthProvider: React.FC = ({ children }) => {
+  // Estado onde será armazenado os dados do usuário dentro da aplicação
+  const [data, setData] = useState<AuthState>({} as AuthState);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadStorageData(): Promise<void> {
+      const [token, user] = await AsyncStorage.multiGet([
+        '@GoBarber:token',
+        '@GoBarber:user',
+      ]);
+
+      if (token[1] && user[1]) {
+        setData({ token: token[1], user: JSON.parse(user[1]) });
+      }
+
+      setLoading(false);
+    }
+
+    loadStorageData();
+  }, []);
+
+  // Context de SignIn
+  const signIn = useCallback(async ({ email, password }) => {
+    const response = await api.post('sessions', {
+      email,
+      password,
+    });
+
+    const { token, user } = response.data;
+
+    await AsyncStorage.multiSet([
+      ['@GoBarber:token', token],
+      ['@GoBarber:user', JSON.stringify(user)],
+    ]);
+
+    setData({ token, user });
+  }, []);
+
+  const signOut = useCallback(async () => {
+    await AsyncStorage.multiRemove(['@GoBarber:token', '@GoBarber:user']);
+
+    setData({} as AuthState);
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user: data.user, signIn, loading, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export function useAuth(): AuthContextData {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+}
